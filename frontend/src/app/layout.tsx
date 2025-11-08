b144544d7909c068077c69dcae600eb81739041e
Cyril Fischer <github@cyril-fischer.fr>
2025-11-08 17:23:39 +0100

    add: Shadcn Components

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import RainbowKitAndWagmiProvider from "@/providers/RainbowKitAndWagmiProvider";
 import Layout from "@/components/shared/Layout";
+import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
             {children}
           </Layout>
         </RainbowKitAndWagmiProvider>
+        <Toaster />
       </body>
     </html>
   );
--- frontend/src/components/shared/SimpleStorage.jsx
+++ frontend/src/components/shared/SimpleStorage.jsx
@@ -1,3 +1,12 @@
+'use client';
+import { useState, useEffect } from "react";
+
+import { toast } from "sonner";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+import { CheckCircle2Icon } from "lucide-react"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 const SimpleStorage = () => {
   return (
     <div>SimpleStorage</div>
