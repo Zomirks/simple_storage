d5b3b59acda9387e837e465840308ae18c3bd52d
Cyril Fischer <github@cyril-fischer.fr>
2025-11-08 17:23:58 +0100

    add: SimpleStorage Imports

--- frontend/src/components/shared/SimpleStorage.jsx
+++ frontend/src/components/shared/SimpleStorage.jsx
@@ -7,6 +7,15 @@ import { Input } from "../ui/input";
 
 import { CheckCircle2Icon } from "lucide-react"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+
+import { contractAddress, contractAbi } from "@/constants";
+
+import { useReadContract, useAccount, useWriteContract, useWaitForTransactionReceipt } from "wagmi";
+
+import { parseAbiItem } from "viem";
+
+import { publicClient } from "@/utils/client";
+
 const SimpleStorage = () => {
   return (
     <div>SimpleStorage</div>
